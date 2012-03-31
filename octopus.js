4b94e69716f5ab33ba7087303a607b2fd0823bd4
unknown <gregg@teenyweeny.(none)>
2012-03-31 15:44:37 -0700

    new images

--- octopus.js
+++ octopus.js
@@ -16,13 +16,31 @@ function getTime() {
   return (new Date()).getTime() * 0.001;
 }
 
-images = {Octopus1:
+images = 
+{
+	bodyHappy:
 	{
-		url: "images/octopus1.jpg",
+		url: "images/octopus_body.jpg"
 	},
-	Octopus2:
+	bodyNormal:
 	{
-		url: "images/octopus2.jpg"
+		url: "images/octopus_body.jpg"
+	},
+	bodyDerp:
+	{
+		url: "images/octopus_body.jpg"
+	},
+	legTip:
+	{
+		url: "images/octopus_leg1.jpg"
+	},
+	legSegment1:
+	{
+		url: "images/octopus_leg2.jpg"
+	},
+	legSegment2:
+	{
+		url: "images/octopus_leg3.jpg"
 	}
 };
 
@@ -48,7 +66,6 @@ function main() {
   
 }
 
-
 function update(elapsedTime) {
   g_ctx.clearRect(0, 0, g_canvas.width, g_canvas.height);
 
@@ -62,7 +79,7 @@ function update(elapsedTime) {
     100,
     "rgb(200,0,255)");
 	
-	g_ctx.drawImage(images.Octopus1.img, 0,0);
+	drawOctopusBody(images.bodyNormal, octoInfo.x, octoInfo.y, octoInfo.rotation, g_ctx);
 }
 
 function drawCircle(ctx, x, y, radius, color) {
@@ -78,6 +95,52 @@ function LoadImage(url, callback)
 	image.onload = callback;
 	image.src = url;
 	return image;
+}
+
+function drawLeg(baseX, baseY, rotation, ctx)
+{
+	//draw tip
+	ctx.save();
+	ctx.drawImage(images.legTip.img, tipX, tipY);
+	base.x = baseX + images.legTip.img.width;
+	base.y = baseY + images.legTip.img.height;
+	ctx.save();
+	ctx.rotate(rotation * .3);
+	ctx.drawImage(images.legSegment1.img, base.x, base.y);
+	ctx.save();
+	ctx.rotate(rotation * .6);
+	ctx.drawImage(images.legSegment2.img, base.x, base.y);
+	ctx.restore();
+	ctx.restore();
+	ctx.restore();
+}
+
+function drawOctopusBody(image, x, y, rotation, ctx)
+{
+	x = x - (image.img.width * .5);
+	y = y - (image.img.height * .5);
+	drawItem(image, x, y, rotation, ctx);
+}
+
+function drawItem(image, x, y, rotation, ctx)
+{
+	ctx.save();
+	ctx.rotate(rotation);
+	ctx.drawImage(image.img, x, y);
+	ctx.restore();
+}
+
+function MoveTentacle(tipPosX, tipPosY)
+{
+	var motionForce = -2;//magnitude
+	//assumes first segment is the tip of the tentacle
+	//move the tip
+	var newPos = 
+	{
+		x: tipPosX - motionForce,
+		y: tipPosY - motionForce
+	};
+	return newPos;
 }
 
 function LoadAllImages(images, callback)
